3a5254fc96ccf4ce66d66c43ee9c718b176f0384
Y-Ismail <ismailyunusk03@gmail.com>
2025-10-22 11:46:31 +0100

    new pages

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-let usertime = document.getElementById('time');
-usertime.innerHTML = setInterval(() =>{
+const usertime = document.getElementById('time');
+usertime.innerHTML = setInterval(() => {
   const date = Date.now();
   usertime.innerHTML = `Time (ms):${date}`
-},1000);
+}, 1000);
+
+
